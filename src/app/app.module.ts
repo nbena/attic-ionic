cf5624ec11a567f29a44e99cbb29377f0e5ca523
nbena <ciaobena@outlook.it>
2017-02-21 21:14:35 +0100

    Very small change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+//mine
 import { NotesPage } from '../pages/notes/notes';
 import { TagsPage } from '../pages/tags/tags';
 
